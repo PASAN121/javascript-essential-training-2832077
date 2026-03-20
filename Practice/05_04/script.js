241c686d2abb9f60662e003e92557615fba76a7d
Pasanchandima123 <PASANCHANDI@GMAIL.COM>
2026-03-20 22:19:38 +0530

    selector and working with the console

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,20 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+const header = document.querySelector(".siteheader");
+
+// Mouse enters
+header.addEventListener("mouseenter", () => {
+  header.style.backgroundColor = "red";
+});
+
+// Mouse leaves
+header.addEventListener("mouseleave", () => {
+  header.style.backgroundColor = "";
+});
+var content = document.querySelectorAll("p");
+
+content.forEach(function (p) {
+  p.style.color = "blue";
+});
